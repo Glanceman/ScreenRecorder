82db3d3abacb83ed8c648abb6a7d973509c82f05
Ben <benxian456@gmail.com>
2023-06-25 22:38:43 +0800

    first commit

---
+++ electron/main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const mode="developement"
+
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration:true,
+      contextIsolation:true,
+    }
+  })
+
+  //win.loadFile('index.html')
+  if(mode==="development"){
+    win.loadURL("http://localhost:3000/")
+  }else{
+    win.loadFile(path.join(__dirname,"../dist/index.html"))
+  }
+
+
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
